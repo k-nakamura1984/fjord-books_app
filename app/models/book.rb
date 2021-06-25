c2a2f9ae313342cc41b6e438535327061ec5318b
k-nakamura1984 <wanonaka76@gmail.com>
2021-06-25 13:25:07 +0900

    i18n first commit

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class Book < ApplicationRecord
+  validates :title, presence: true
+  validates :memo, presence: true
   mount_uploader :picture, PictureUploader
 end
