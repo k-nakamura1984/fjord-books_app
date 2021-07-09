fd9e03139d8438c42864d9691a49ad1896ec0933
k-nakamura1984 <wanonaka76@gmail.com>
2021-07-09 14:23:34 +0900

    ActiveStorageのプラクティスに際して、下記を実装しました。
    画像のリサイズ。

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :icon
+  # validates :icon, attached_file_type: { pattern: /^image\// }
 end
