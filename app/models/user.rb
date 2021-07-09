2ce7da15ca16c29e03fa23ab1c82ef39a5272ec7
k-nakamura1984 <wanonaka76@gmail.com>
2021-07-09 20:09:44 +0900

    ActiveStorageプラクティスのため、下記を実装しました。
    ユーザー編集画面にアイコン画像を表示。
    特定の拡張子以外のファイルをアップロードした際にエラーを表示。
    ファイルのアップロード容量を1MBまでに制限。

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_one_attached :icon
   # validates :icon, attached_file_type: { pattern: /^image\// }
+
+  validate :icon_type, :icon_size
+
+  private
+
+  def icon_type
+    errors.add(:icon, :invalid_file_type) unless icon.blob.content_type.in?(%('image/jpg image/jpeg image/png image/gif'))
+  end
+
+  def icon_size
+    errors.add(:icon, :invalid_file_size) if icon.blob.byte_size > 1.megabytes
+  end
 end
