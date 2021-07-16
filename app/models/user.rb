227a8ae85f7f401cfa512108fe6af528aef25a1e
k-nakamura1984 <wanonaka76@gmail.com>
2021-07-16 17:51:06 +0900

    「Railsでユーザーフォローを作る」プラクティスの課題を提出します。
    ・中間テーブルとしてRelationshipテーブルを作成。
    ・ユーザー詳細画面にフォロー・フォロー解除ボタンを作成。
    ・ユーザー詳細画面から、フォロー一覧とフォロワー一覧へのリンクを作成。
    ・追加事項のi18nを対応。
    ・ユーザーが退会（アカウント削除）した際に、dependent: :destoryで関連するデータベースも削除するように設定。

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,18 +2,26 @@
 
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: %i[github]
+         :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
 
-  validates :uid, uniqueness: { scope: :provider }, if: -> { uid.present? }
+  has_many :followers, class_name: 'Relationship',
+                       foreign_key: 'follower_id',
+                       dependent: :destroy,
+                       inverse_of: :follower
+  has_many :followings, class_name: 'Relationship',
+                        foreign_key: 'followed_id',
+                        dependent: :destroy,
+                        inverse_of: :followed
+  has_many :following_users, through: :followers, source: :followed
+  has_many :follower_users, through: :followings, source: :follower
 
-  def self.from_omniauth(auth)
-    find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
-      user.name = auth.info.name
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-    end
+  def follow(user_id)
+    followers.create(followed_id: user_id)
+  end
+
+  def following?(user)
+    following_users.include?(user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,15 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: "users/registrations" }
   root to: 'books#index'
+
   resources :books
-  resources :users, only: %i(index show)
+
+  resources :users do
+    member do
+      get :followings, :followers
+    end
+  end
+
+  resources :relationships, only: %i[create destroy]
+
 end
