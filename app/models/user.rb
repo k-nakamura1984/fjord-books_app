65f66e2671aadc0cd047f4eb990a33a602507cd9
k-nakamura1984 <wanonaka76@gmail.com>
2021-07-19 19:11:41 +0900

    ActiveStorageプラクティスのため、下記を実装しました。
    iconの登録がない場合は、バリデーションの対象外にするよう変更。
    削除していたマイグレーションファイルをrails active_storage:installすることにより再生成し、migrationとschema.rbを整合。

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :icon
-  # validates :icon, attached_file_type: { pattern: /^image\// }
 
   validate :icon_type, :icon_size
 
   private
 
   def icon_type
-    errors.add(:icon, :invalid_file_type) unless icon.blob.content_type.in?(%('image/jpg image/jpeg image/png image/gif'))
+    errors.add(:icon, :invalid_file_type) if icon.blob.present? && !icon.blob.content_type.in?(%('image/jpg image/jpeg image/png image/gif'))
   end
 
   def icon_size
-    errors.add(:icon, :invalid_file_size) if icon.blob.byte_size > 1.megabytes
+    errors.add(:icon, :invalid_file_size) if icon.blob.present? && (icon.blob.byte_size > 1.megabytes)
   end
 end
