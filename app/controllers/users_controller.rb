d6f947050e619d86b8e09098050f7e0de7b2db18
k-nakamura1984 <wanonaka76@gmail.com>
2021-07-21 11:55:48 +0900

    N+1問題の対策のため、with_attached_attachment_nameを追記しました

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@
 
 class UsersController < ApplicationController
   def index
-    @users = User.order(:id).page(params[:page])
+    # @users = User.order(:id).page(params[:page])
+    @users = User.with_attached_icon.order(:id).page(params[:page])
   end
 
   def show
